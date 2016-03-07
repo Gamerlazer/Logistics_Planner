3a143e649b1a0c5835bdbc0a4c2b106de0ff02aa
Gamerlazer <gamerlazer@gmail.com>
2016-03-06 23:22:14 -0800

    shorten api keys

--- api_testing.py
+++ api_testing.py
@@ -1,7 +1,7 @@
 
 import rauth
 
-# obtain yelp api keys
+# Obtain yelp api keys
 with open(".gitignore") as yelp_api:
 	yelp_keys = yelp_api.readlines()
 
@@ -9,28 +9,17 @@ yelp_api_keys = []
 for k in yelp_keys:
 	yelp_api_keys.append(k.split(':')[1].replace('\n',''))
 
-consumer_key = yelp_api_keys[0]
-consumer_secret = yelp_api_keys[1]
-token = yelp_api_keys[2]
-token_secret = yelp_api_keys[3]
-
-print yelp_api_keys
-
 def get_results(params):
 
 	session = rauth.OAuth1Session(
-	consumer_key = consumer_key
-	,consumer_secret = consumer_secret
-	,access_token = token
-	,access_token_secret = token_secret)
+	consumer_key = yelp_api_keys[0],consumer_secret = yelp_api_keys[1],access_token = yelp_api_keys[2],access_token_secret = yelp_api_keys[3])
 	 
 	request = session.get("http://api.yelp.com/v2/search",params=params)
 
 	#Transforms the JSON API response into a Python dictionary
 	data = request.json()
-	
-
 	return data
+
 
 
 def get_search_parameters(lat,long):
